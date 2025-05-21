fa96c8bcdc4211cbc0910111bfbef67c9c3697ee
poprostuadam <adam.stefcio@gmail.com>
2025-05-21 19:45:35 +0200

    unit tests

---
+++ ecommerce-backend/test/Model/ProductTest.java
@@ -0,0 +1,19 @@
+package Model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class ProductTest {
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void isAvailable() {
+    }
+
+    @Test
+    void setCategory() {
+    }
+}
